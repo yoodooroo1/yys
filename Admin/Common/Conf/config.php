8f7a593041424f6fcac69cc0868973820e0d712c
ydr <541911483@qq.com>
2020-02-05 18:05:41 +0800

    feat: 代码上传(未完成)

--- Admin/Common/Conf/config.php
+++ Admin/Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
     //'配置项'=>'配置值'
     'URL_CASE_INSENSITIVE'  =>  true,
     'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'127.0.0.1',//120.25.200.1 118.89.53.225
+    'DB_HOST'=>'47.100.137.134',//120.25.200.1 118.89.53.225
     'DB_NAME'=>'yys',//xihuansha
-    'DB_USER'=>'root',//zbc
-    'DB_PWD'=>'123456', //zbc5133 //Wjd8988998
+    'DB_USER'=>'yys',//zbc
+    'DB_PWD'=>'t66zRHMBxjXGMWFB', //zbc5133 //Wjd8988998
     'DB_PREFIX'=>'lm_',
     'IMG_ROOT_PATH'=>'./images/',
 );
